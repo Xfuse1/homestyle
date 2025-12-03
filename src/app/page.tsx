86d16b4516beaed26c73cc393278e76ec46f172b
CVEEEZ GMAIL <cveeezg@gmail.com>
2025-12-03 11:48:36 +0000

    https://i.postimg.cc/XJv4KdYY/abajwrh-tyst.png
    ضع الصوره الموجوده في هذ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -198,7 +198,7 @@ export default function Home() {
             </div>
             <div className="lg:order-1 flex items-center justify-center" data-aos="fade-right">
               <Image
-                src="https://i.postimg.cc/3rtqPzJw/abajwrh-faynal.jpg"
+                src="https://i.postimg.cc/XJv4KdYY/abajwrh-tyst.png"
                 alt="Elegant floor lamp"
                 width={800}
                 height={1200}
